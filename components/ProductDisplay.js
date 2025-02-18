480b8b3883205e75a6f91586bfda585e66bef326
MatteoA-30201 <125mamatucci@frhsd.com>
2025-02-18 08:33:43 -0500

    lesson 11 completed

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -30,6 +30,8 @@ app.component('product-display', {
             <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" v-on:click="addToCart">Add to Cart</button>
           </div>
         </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
       </div>`,
       data() {
         return {
@@ -40,7 +42,8 @@ app.component('product-display', {
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
+            ],
+            reviews: []
          }
       },
       methods: {
@@ -50,6 +53,9 @@ app.component('product-display', {
             updateVariant(index) {
               this.selectedVariant = index
             },
+            addReview(review) {
+              this.reviews.push(review)
+            }
         },
         computed: {
           title() {
